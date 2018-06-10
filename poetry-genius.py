52d5ad6c63f1404dd34ee14d900b469e532fbba7
Mitchell Donahue <mrdonee@umich.edu>
2018-06-10 10:00:49 -0700

    Added a layer to LSTM, CONSTS, spell check

--- poetry-genius.py
+++ poetry-genius.py
@@ -1,5 +1,6 @@
 import numpy
 import sys
+from autocorrect import spell
 from keras.callbacks import ModelCheckpoint
 from keras.models import Sequential
 from keras.layers import Dense
@@ -8,10 +9,18 @@ from keras.layers import LSTM
 from keras.utils import np_utils
 from os import listdir
 from os.path import isfile, join
+import re
 
-inFile = "./inputFiles/wonderland.txt"
-rawText = open(inFile).read()
+CONST_IN_FILE = "./inputFiles/sonnets.txt"
+CONST_RM_PUNCT = True
+CONST_TRAIN = False
+CONST_NUM_EPOCHS = 100
+CONST_BATCH_SIZE = 64
+
+rawText = open(CONST_IN_FILE).read()
 rawText = rawText.lower()
+if (CONST_RM_PUNCT):
+	rawText = re.sub(r'[^\w\s]','', rawText)
 
 # create map of all chars found in text and reverse lookup table
 chars = sorted(list(set(rawText)))
@@ -37,38 +46,39 @@ numPatterns = len(charSequences)
 print "Total Patterns: ", numPatterns
 
 # reshape charSequences to be [samples, time steps, features]
-reshapedCharSeqs = numpy.reshape(charSequences, (numPatterns, seqLength, 1))
+charSeqs = numpy.reshape(charSequences, (numPatterns, seqLength, 1))
 
 # normalize
-reshapedCharSeqs = reshapedCharSeqs / float(numVocab)
+charSeqs = charSeqs / float(numVocab)
 
 # one hot encode the output variable
 oneHotNextChar = np_utils.to_categorical(nextChar)
 
 # define the LSTM model
 model = Sequential()
-model.add(LSTM(256, input_shape=(reshapedCharSeqs.shape[1], reshapedCharSeqs.shape[2]), return_sequences=True))
+model.add(LSTM(256, input_shape=(charSeqs.shape[1], charSeqs.shape[2]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256, return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(oneHotNextChar.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-# define the checkpoint
-filepath="./models/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacksList = [checkpoint]
+# define the checkpoint and fit the model
+if CONST_TRAIN:
+	filepath="./models/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+	checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+	callbacksList = [checkpoint]
+	model.fit(charSeqs, oneHotNextChar, epochs=CONST_NUM_EPOCHS, batch_size=CONST_BATCH_SIZE, callbacks=callbacksList)
 
-# fit the model
-model.fit(reshapedCharSeqs, oneHotNextChar, epochs=20, batch_size=128, callbacks=callbacksList)
+# load network weights
 weightImprovePath = "./models/"
 onlyfiles = [f for f in listdir(weightImprovePath) if isfile(join(weightImprovePath, f))]
 networkWeightFile = ""
 for item in onlyfiles[:]:
     if item.startswith("weights-improvement-"):
         networkWeightFile = item
-
-# load network weights
 model.load_weights(weightImprovePath + networkWeightFile)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
@@ -79,14 +89,27 @@ print "Starting Sequence:"
 print "\"", ''.join([intToChar[value] for value in pattern]), "\""
 
 # generate output
+poem = []
+currLine = ""
 for i in range(1000):
 	reshapedPattern = numpy.reshape(pattern, (1, len(pattern), 1))
 	reshapedPattern = reshapedPattern / float(numVocab)
 	prediction = model.predict(reshapedPattern, verbose=0)
 	index = numpy.argmax(prediction)
-	result = intToChar[index]
-	seqIn = [intToChar[value] for value in pattern]
-	sys.stdout.write(result)
+	if (intToChar[index] == "\n"):
+		poem.append(currLine)
+		currLine = ""
+	else:
+		currLine += intToChar[index]
 	pattern.append(index)
 	pattern = pattern[1:len(pattern)]
-print "\nDone."
+
+# spell check output
+spelledCheckedLine = ""
+for line in poem:
+	lineWords = line.split()
+	for word in lineWords:
+		spelledCheckedLine += spell(word) + " "
+	print(spelledCheckedLine)
+	spelledCheckedLine = ""
+print "\nDone"
